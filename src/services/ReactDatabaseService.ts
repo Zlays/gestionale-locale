e5e1b62f601aba816db20d89710ee30ec6e8d15d
Mirko <mirkobuccella@gmail.com>
2022-12-30 23:07:44 +0100

    several changes

--- src/services/ReactDatabaseService.ts
+++ src/services/ReactDatabaseService.ts
@@ -1,69 +1,100 @@
 import { Imovements, Iproject } from '../utils/DbInterface';
 
-export default function sendAsync(sql) {
+export default function sendAsync(channel: string, sql: string) {
   return new Promise((resolve) => {
-    window.electron.ipcRenderer.once('asynchronous-reply', (_, arg) => {
+    window.electron.ipcRenderer.once(channel, (_, arg) => {
       resolve(_);
     });
-    window.electron.ipcRenderer.sendMessage('asynchronous-message', sql);
+    window.electron.ipcRenderer.sendMessage(`db-query`, [channel, sql]);
   });
 }
 
 function getAllProjects() {
-  return sendAsync(`SELECT *
-                    FROM project_infos`);
-}
-
-function addProject(name: string, nominative_value: number, date: string) {
-  const values = `'${name}','${nominative_value}','${date}'`;
-  return sendAsync(`INSERT INTO projects(name, nominative_value, date)
-                    VALUES (${values})`);
-}
-
-function editProject(project: Iproject) {
-  return sendAsync(`UPDATE projects
-                    SET name = '${project.name}',
-                        nominative_value = '${project.nominative_value}',
-                        date = '${project.date}'
-                    WHERE id = ${project.id}`);
-}
-
-function removeProject(id: number) {
-  return sendAsync(`DELETE
-                    FROM projects
-                    WHERE id = ${id};`);
-}
-
-function getMovementsByGroup(idProject: number) {
-  return sendAsync(`SELECT *
-                    FROM movements
-                    where id_project = ${idProject}`);
-}
-
-function addMovement(movement: Imovements) {
-  const values = `'${movement.value}','${movement.date}','${movement.description}','${movement.idProject}'`;
   return sendAsync(
-    `INSERT INTO movements(value, date, description, id_project)
-     VALUES (${values})`
+    'getAllProjects',
+    `SELECT *
+                    FROM project_infos`
   );
 }
 
+function getProjectById(idProject: number) {
+  const sqlScript = `SELECT *
+                    FROM project_infos
+                    WHERE id=${idProject} `;
+  return sendAsync('getProjectById', sqlScript);
+}
+
+function addProject(
+  name: string,
+  nominative_value: number,
+  startDate: string,
+  endDate: string
+) {
+  const values = `'${name}','${nominative_value}','${startDate}','${endDate}'`;
+  const sqlScript = `INSERT INTO projects(name, nominative_value, start_date, end_date)
+                    VALUES (${values})`;
+  return sendAsync('addProject', sqlScript);
+}
+
+function editProject(project: Iproject) {
+  const sqlScript = `UPDATE projects
+                    SET name = '${project.name}',
+                        nominative_value = '${project.nominative_value}',
+                        start_date = '${project.start_date}',
+                        end_date = '${project.end_date}'
+                    WHERE id = ${project.id}`;
+  return sendAsync('editProject', sqlScript);
+}
+
+function removeProject(id: number) {
+  return sendAsync(
+    'removeProject',
+    `DELETE
+                    FROM projects
+                    WHERE id = ${id};`
+  );
+}
+
+function getMovementsByGroup(idProject: number) {
+  return sendAsync(
+    'getMovementsByGroup',
+    `SELECT *
+                    FROM movements
+                    where id_project = ${idProject}`
+  );
+}
+
+function addMovement(movement: Imovements) {
+  const values = `'${movement.value}','${movement.date}','${movement.description}','${movement.idProject}','${movement.order}'`;
+  const sqlScript = `INSERT INTO movements(value, date, description, id_project, 'order')
+     VALUES (${values})`;
+  return sendAsync('addMovement', sqlScript);
+}
+
 function editMovements(movement: Imovements) {
-  return sendAsync(`UPDATE movements
+  return sendAsync(
+    'editMovements',
+    `UPDATE movements
                     SET description = '${movement.description}',
                         date        = '${movement.date}',
-                        value       = '${movement.value}'
-                    WHERE id = ${movement.id}`);
+                        value       = '${movement.value}',
+                        'order'       = '${movement.order}'
+                    WHERE id = ${movement.id}`
+  );
 }
 
 function removeMovement(id: number) {
-  return sendAsync(`DELETE
+  return sendAsync(
+    'removeMovement',
+    `DELETE
                     FROM movements
-                    WHERE id = ${id};`);
+                    WHERE id = ${id};`
+  );
 }
 
 export {
   getAllProjects,
+  getProjectById,
   addProject,
   editProject,
   removeProject,
--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -1,5 +1,11 @@
-function sleep(time) {
+import moment from 'moment';
+
+function sleep(time: number) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-export { sleep };
+function formatDate(value: string) {
+  return moment(value).format('DD/MM/yyyy');
+}
+
+export { sleep, formatDate };
